8c432b3ed1006d4a0a5b73f4d1d7bbb33941a0d9
Sebastian Lohmeier <sl@monochromata.de>
2015-12-08 17:19:16 +0100

    refactoring

--- de.monochromata.jactr.twm/models/de/monochromata/jactr/rm/ScopedRetrievalModule6.java
+++ de.monochromata.jactr.twm/models/de/monochromata/jactr/rm/ScopedRetrievalModule6.java
@@ -12,7 +12,7 @@ import de.monochromata.jactr.twm.ITWM;
 
 /**
  * A retrieval module that limits the chunks to be returned by a
- * retrieval to scope that are identical to a set scope or enclose
+ * retrieval to scopes that are identical to a set scope or enclose
  * the set scope.
  *
  * @see Scope#isIdenticalOrEncloses(Scope)
@@ -32,24 +32,26 @@ public class ScopedRetrievalModule6 extends DefaultRetrievalModule6
 	protected IChunk selectRetrieval(Collection<IChunk> results,
 			IChunk errorChunk, ChunkTypeRequest originalRequest,
 			ChunkTypeRequest cleanedRequest) {
-		
-		// If a scope is in effect, restrict results to chunks
-		// from identical or enclosing scopes.
 		if(this.scope != null) {
-			Iterator<IChunk> iterator = results.iterator();
-			while(iterator.hasNext()) {
-				IChunk chunk = iterator.next();
-				Scope scope = (Scope)chunk.getMetaData(ITWM.scope);
-				if(scope == null)
-					throw new IllegalStateException("Chunk has no scope: "+chunk);
-				if(!scope.isIdenticalOrEncloses(this.scope))
-					iterator.remove();
-			}
+			restrictResultsToChunksFromIdenticalOrEnclosingScopes(results);
 		}
-		
-		// Select the chunk to return
 		return super.selectRetrieval(results, errorChunk, originalRequest,
 				cleanedRequest);
+	}
+
+	protected void restrictResultsToChunksFromIdenticalOrEnclosingScopes(Collection<IChunk> results) {
+		Iterator<IChunk> iterator = results.iterator();
+		while(iterator.hasNext()) {
+			removeChunkIfFromScopeThatIsNotEnclosedAndIsNotIdentical(iterator, iterator.next());
+		}
+	}
+
+	protected void removeChunkIfFromScopeThatIsNotEnclosedAndIsNotIdentical(Iterator<IChunk> iterator, IChunk chunk) {
+		Scope chunkScope = (Scope)chunk.getMetaData(ITWM.scope);
+		if(chunkScope == null)
+			throw new IllegalStateException("Chunk has no scope: "+chunk);
+		if(!chunkScope.isIdenticalOrEncloses(this.scope))
+			iterator.remove();
 	}
 
 }
